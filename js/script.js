1e1f5d45a169ee9f34d4632b6dfa07723341c1e8
WolfSa13 <hardcore2991@gmail.com>
2022-04-06 13:31:45 +0300

    finish details

--- js/script.js
+++ js/script.js
@@ -1,19 +1,72 @@
-const sliderThumbs = new Swiper('.slider-thumbs', {
-    loop: true,
-    spaceBetween: 20,
-    slidesPerView: 3,
-    centeredSlides: true
+// Burger menu
+const burger = document.querySelector('.burger')
+const navigation = document.querySelector('.navigation');
+const navigationClose  = document.querySelector('.navigation__close');
+
+burger.addEventListener('click', () => {
+    navigation.classList.add('navigation_active');
 });
 
-sliderThumbs.on('click', (swiper)=> {
-    swiper.slideTo(swiper.clickedIndex)
+navigationClose.addEventListener('click', () => {
+    navigation.classList.remove('navigation_active');
+});
+
+// music
+try {
+    const mute = document.querySelector('.mute__checkbox');
+const audio = new Audio('audio/waterTower.mp3')
+const checkMute = () => {
+    if(mute.checked) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+};
+if (mute) {
+setTimeout(checkMute, 2000)
+}
+
+mute.addEventListener('change', checkMute)
+} catch {
+    console.log('No music in this page')
+}
+
+try {
+    const sliderThumbs = new Swiper('.slider-thumbs', {
+        loop: true,
+        spaceBetween: 20,
+        slidesPerView: 3,
+        centeredSlides: true
+    });
+
+    sliderThumbs.on('click', (swiper)=> {
+        swiper.slideTo(swiper.clickedIndex)
+    })
+
+    const sliderMain = new Swiper('.slider-main', {
+        loop: true,
+        spaceBetween: 10,
+        loopedSlides: 4,
+    });
+
+    sliderThumbs.controller.control = sliderMain;
+    sliderMain.controller.control = sliderThumbs;
+
+    const video = document.querySelectorAll('video');
+
+    sliderMain.on('slideChange', () => {
+        for(let i = 0; i < videos.length; i += 1){
+            videos[i].pause();
+        }
+    });
+
+const pagination = document.querySelector('.pagination');
+const paginationButton = document.querySelector('.pagination__arrow');
+
+paginationButton.addEventListener('click', () => {
+    pagination.classList.toggle('pagination_active')
 })
 
-const sliderMain = new Swiper('.slider-main', {
-    loop: true,
-    spaceBetween: 10,
-    loopedSlides: 6,
-});
-
-sliderThumbs.controller.control = sliderMain;
-sliderMain.controller.control = sliderThumbs;
+} catch {
+   
+}
